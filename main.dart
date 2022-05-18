9f96a2ec457434c600afe1625de4124fbcb46647
minaEmadRadi <36459128+minaEmadRadi@users.noreply.github.com>
2022-05-18 10:08:40 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:qr_scanner/ui/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "QR Scanner",
+    home: Home(),theme: ThemeData(primaryColor: Colors.teal),
+  ));
+}
